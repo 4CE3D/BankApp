30319f04be692d1d678e698f0b19705addc651c7
4CE3D <andrejssidorovs@gmail.com>
2024-04-02 16:08:54 +0300

    test template

---
+++ src/BankAppTest.java
@@ -0,0 +1,56 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+
+class BankAppTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addAccount() {
+        assertEquals(1,1);
+    }
+
+    @org.junit.jupiter.api.Test
+    void viewAllAccounts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchAccount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void accountMenu() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void viewBalance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void transfer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void callID() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveAccountsToFile() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loadAccountsFromFile() {
+    }
+}
